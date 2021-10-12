b529d29dddfa63131463293f5113983de71c9223
wellington197 <fcowellcosta@gmail.com>
2021-10-12 11:12:00 -0300

    When executing this commit, the scroll button and the js file that performs the command to upload the project will be created.

--- static/js/owl/botaosubir.js
+++ static/js/owl/botaosubir.js
@@ -6,3 +6,19 @@ function subirTela() {
 
     });
 }
+
+
+function decidirBotaoScroll() {
+    if (window.scrollY === 0) {
+        //ocultar botão
+        document.querySelector('.scrollbutton').style.display = 'none';
+
+    } else {
+        //mostrar botão
+        document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
+
+/*irá monitorar qualquer movimento do scroll da tela*/
+
+window.addEventListener('scroll', decidirBotaoScroll);
